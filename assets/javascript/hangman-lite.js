720d886a38003c15315b2ea8b6a9f89eedf6a2cb
Suter <sutert@amazon.com>
2019-09-24 16:41:26 -0400

    update hangman-lite

--- assets/javascript/hangman-lite.js
+++ assets/javascript/hangman-lite.js
@@ -1,18 +1,20 @@
 (function(){
-
-    /*Create a variable called word that contains a hangman word, 
-    and create a second variable called letter that indicates the 
-    letter that the player guessed. Print out "Yeah, the letter x 
-    exists y times in my word" if the letter exists (but substitute the 
-    actual letter and the number of times). If the letter doesn't 
-    exist, print out "Nope, that letter doesn't exist in my word." */
     
    let word = "Oceaniana";
-   let letter = "s"
+    let letter = "a"
+    let  count = 0
+    for (let i=0; i < word.length; i++){
+        if(letter == word[i]){
+            count++;
+        }
+    }
+    
+    let x =letter
+    let y = console.log(count)
 
-   let result = word.indexOf(letter)
-    if (result == -1){console.log("Not Present")} 
-    else {console.log("Present")}
-
+    if(console.log(count) >= 1 ){console.log(Yeah, the letter x 
+    exists y times in my word)}
+    else {console.log(Nope, that letter does not exist in my word)}
+}
    
 })();
